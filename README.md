59c0b4c2abf73f88fdc5e7b44deeb17902741ead
ww <weiwei@snqu.com>
2020-02-24 17:28:53 +0800

    添加注释

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# node-server" 
+#express 框架和 mysql数据库
+> 使用express 框架和mysql 数据库搭建的后台服务
+> 实现了最基础的增删改查
--- config/dbHelp.js
+++ config/dbHelp.js
@@ -1,5 +1,19 @@
 let mysql = require('mysql')
 let db_config = require ('../config/dbConfig')
+/**
+ * dbConfig 是一个配置文件, 因为有数据库敏感信息, 所以没有提交, 文件内容如下
+ * const config = {
+    database: {
+      database: "数据库名称",
+      user: "账号",
+      password: "密码",
+      host: "数据库地址",
+      port: "数据库端口",
+      insecureAuth: true,
+      useConnectionPooling: true
+    }
+  };
+ */
 // 数据库链接池
 let pool = mysql.createPool({
   host: db_config.database.host,
